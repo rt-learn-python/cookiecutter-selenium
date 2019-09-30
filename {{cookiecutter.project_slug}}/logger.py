9acb5f631b0a1fb3fd9c82b4fe3d83474cb59fd1
Royce Remulla <royce.com@gmail.com>
2019-09-30 14:18:17 +0800

    Fix escaping logger mustaches

--- {{cookiecutter.project_slug}}/logger.py
+++ {{cookiecutter.project_slug}}/logger.py
@@ -1,15 +1,16 @@
 import logging
 
-instance = logging.getLogger('ticket_manager')
+instance = logging.getLogger('{{cookiecutter.project_slug}}')
 instance.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
+{% raw %}
 formatter = logging.Formatter(
     '%(asctime)s - %(levelname)s {%(pathname)s:%(lineno)d} - %(message)s',
     '%Y-%m-%d %H:%M:%S')
-
+{% endraw %}
 
 # add formatter to ch
 ch.setFormatter(formatter)
